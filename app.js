7b4970bdfe595b21c769527e8150dc0cfd83d85d
nameismani <120552594+nameismani@users.noreply.github.com>
2023-01-07 17:21:38 +0530

    Created html css and js file

---
+++ app.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    //Hero Slider
+    // $('#hero-slider').owlCarousel({
+    //     loop:true,
+    //     margin:10,
+    //     nav:true,
+    //     items:1,
+    //     responsive:{
+    //         0:{
+            
+    //         },
+    //         600:{
+            
+    //         },
+    //         1000:{
+                
+    //         }
+    //     }
+    // })
+    
+    // $('.owl-carousel').owlCarousel({
+    //     loop:true,
+    //     margin:10,
+    //     nav:false,
+    //     dots:true,
+       
+    // })
+    
+ });
